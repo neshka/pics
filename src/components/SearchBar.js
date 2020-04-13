fac3c54cf1ddae49cecd0bfc9645401cafbbec36
AgnieszkaZuchowicz <agnieszka.zuchowicz@b2m-solutions.com>
2020-04-13 13:02:35 +0100

    add SearchBar component with simple form text

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>)
+    }
+}
+
+export default SearchBar;
