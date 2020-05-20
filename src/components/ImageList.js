5d704e3b4bdb3b702d8bb8299315dc1a7a019dfe
AgnieszkaZuchowicz <agnieszka.zuchowicz@b2m-solutions.com>
2020-05-20 22:21:05 +0100

    show images in imagelist component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import moment from 'moment';
 
 const ImageList = (props) => {
+    console.log(props.images)
+    const images = props.images.map((image) => {
+        return <img src={image.urls.regular} />
+    })
 
-    return <div>ImageList</div>;
+    return <div>{images}</div>;
 };
 
 export default ImageList;
