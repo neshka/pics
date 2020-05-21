c2409eeb1034aeca9498e18762aed1d43c05c347
AgnieszkaZuchowicz <agnieszka.zuchowicz@b2m-solutions.com>
2020-05-21 11:25:03 +0100

    add key and alt to img

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images)
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+    const images = props.images.map(({ description, id, urls }) => {
+        return <img alt={description} key={id} src={urls.regular} />
     })
 
     return <div>{images}</div>;
