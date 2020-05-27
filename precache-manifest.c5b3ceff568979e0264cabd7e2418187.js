66ada1c47e8b8cb55679fe60a39de759cb9d9d93
AgnieszkaZuchowicz <agnieszka.zuchowicz@b2m-solutions.com>
2020-05-27 21:35:07 +0100

    Updates

---
+++ precache-manifest.c5b3ceff568979e0264cabd7e2418187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59439c3e2be86e173b569330f54d4bdb",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "0418c80a8bddf1967ee1",
+    "url": "/pics/static/css/main.465765bc.chunk.css"
+  },
+  {
+    "revision": "576239bdd47165c3f765",
+    "url": "/pics/static/js/2.f88abdb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.f88abdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0418c80a8bddf1967ee1",
+    "url": "/pics/static/js/main.0f736eda.chunk.js"
+  },
+  {
+    "revision": "1d26167a8e6ba9914b1c",
+    "url": "/pics/static/js/runtime-main.ee210543.js"
+  }
+]);
--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization:
-            'Client-ID GgZd-sVW_t5af35fbrTi2nEdDlSV1riVeTMuh69s7F4'
-    }
-});
